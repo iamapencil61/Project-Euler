966a0f4efc5738685d0c2d07a765b3a0912d1f1f
Emre <84818883+iamapencil61@users.noreply.github.com>
2023-05-30 21:09:56 +0300

    Problem719.py

--- ProjectEuler/Problem719.py
+++ ProjectEuler/Problem719.py
@@ -1,75 +1,71 @@
 import time, math
+baslangic_zamani = time.time()
 
-başlangıç_zamanı = time.time()
-
-
-def liste_oluşturucu(bir_sayı):
-    uzunluk = len(str(bir_sayı))
+def liste_olusturucu(bir_sayi):
+    uzunluk = len(str(bir_sayi))
     dizi = []
-    for x in range(uzunluk + 1):
+    for x in range(uzunluk+1):
         dizi.append([])
-
-    yığın = [bir_sayı]
-    while len(yığın) != 0:
-        şimdiki = yığın.pop(0)
-        dizi[len(str(şimdiki))].append(şimdiki)
-
-        if len(str(şimdiki)) == 1:
+    
+    yigin = [bir_sayi]
+    while len(yigin) != 0:
+        simdiki = yigin.pop(0)
+        dizi[len(str(simdiki))].append(simdiki)
+        
+        if len(str(simdiki)) == 1:
             continue
-
-        ilk_haneden_kaldır = int(str(şimdiki)[1:])
-        son_haneden_kaldır = şimdiki // 10
-
-        yığın.append(ilk_haneden_kaldır)
-        yığın.append(son_haneden_kaldır)
-
-    for x in range(uzunluk + 1):
+        
+        ilk_haneden_kaldir = int(str(simdiki)[1:])
+        son_haneden_kaldir = simdiki//10
+        
+        yigin.append(ilk_haneden_kaldir)
+        yigin.append(son_haneden_kaldir)
+    
+    for x in range(uzunluk+1):
         dizi[x] = list(set(dizi[x]))
-    return dizi
-
-
-def bölümle(bir_sayı):
-    cevap = set()
-    cevap.add((bir_sayı,))
-    for x in range(1, bir_sayı):
-        for y in bölümle(bir_sayı - x):
-            cevap.add(tuple(((x,) + y)))
-
-    return sorted([x for x in cevap])
-
-
-def hesapla(sınır):
+    return dizi 
+        
+def bolumle(bir_sayi):
+     cevap = set()
+     cevap.add((bir_sayi, ))
+     for x in range(1, bir_sayi):
+         for y in bolumle(bir_sayi - x):
+             cevap.add(tuple(((x, ) + y)))
+        
+     return sorted([x for x in cevap])
+    
+def hesapla(sinir):
+    
     toplam = 0
-
-    for z in range(1, sınır + 1):
+    
+    for z in range(1, sinir+1):
         if z % 100000 == 0:
             print(z)
             print(toplam)
-            print("--- %s saniye ---" % (time.time() - başlangıç_zamanı))
+            print("--- %s saniye ---" % (time.time() - baslangic_zamani))
         if z % 9 == 0 or z % 9 == 1:
-            x = z ** 2
-            geçici_liste = bölümle(len(str(x)))[:-1]
-
-            for y in geçici_liste:
-                geçici_numara = str(x)
-                geçici_toplam = 0
-                değer = ""
-
+            x = z**2
+            gecici_liste = bolumle(len(str(x)))[:-1]
+            
+            for y in gecici_liste:
+                gecici_numara = str(x)
+                gecici_toplam = 0
+                deger = ""
+                
                 for z in y:
-                    değer += geçici_numara[:z]
+                    deger += gecici_numara[:z]
                     try:
-                        geçici_toplam += int(geçici_numara[:z])
+                        gecici_toplam += int(gecici_numara[:z])
                     except ValueError:
                         pass
-                    geçici_numara = geçici_numara[z:]
-                    # print(değer)
-
-                if değer == str(geçici_toplam ** 2):
+                    gecici_numara = gecici_numara[z:]
+                    #print(deger)
+                    
+                if deger == str(gecici_toplam**2):
                     toplam += x
                     break
     return toplam
-
-
+    
 if __name__ == "__main__":
-    print(hesapla(10 ** 6))
-    print("--- %s saniye ---" % (time.time() - başlangıç_zamanı))
+    print(hesapla(10**6))
+    print("--- %s saniye ---" % (time.time() - baslangic_zamani))
